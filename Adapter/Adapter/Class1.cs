b12f8ebf6881b8cc7a7cc49fd9321e64a4916bf7
bogdanborzianytsia <128235605+bogdanborzianytsia@users.noreply.github.com>
2023-03-23 20:44:02 +0200

    Add files via upload

---
+++ Adapter/Adapter/Class1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace gonki
+{
+    interface IPhoto
+    {
+        void print();
+
+    }
+
+    class Photo : IPhoto
+    {
+        public string PhotoType { get; set; }
+        public string prin, name;    
+        public void print()
+        {
+            prin = name + " have type " + PhotoType;
+        }
+    }
+    class BitmapAdapter : Photo
+    {
+        private Photo photo;
+
+        public BitmapAdapter(Photo phot)
+        {
+            photo = phot;
+            name = "BitmapPhoto";
+            PhotoType = "Bitmap";
+
+        }
+        public void print()
+        {
+
+            photo.print();
+            prin = name + " have type " + PhotoType;
+
+        }
+
+    }
+    class JPEGAdapter : Photo
+    {
+        private Photo photo;
+
+        public JPEGAdapter(Photo phot)
+        {
+            photo = phot;
+            name = "JPEGphoto";
+            PhotoType = "JPEG";
+        }
+
+        public void print()
+        {
+            photo.print();
+            prin = name + " have type " + PhotoType;
+        }
+    }
+
+    class PNGAdapter : Photo
+    {
+        private Photo photo;
+
+        public PNGAdapter(Photo phot)
+        {
+            photo = phot;
+            name = "PNGphoto";
+            prin = name + " have type " + PhotoType;
+
+        }
+        public void print()
+        {
+            photo.print();
+            prin = name + " have type " + PhotoType;
+        }
+    }
+    class VectorAdapter : Photo
+    {
+        private Photo photo;
+        public VectorAdapter(Photo phot)
+        {
+            photo = phot;
+            name = "VectorPhoto";
+            PhotoType = "Vector";
+        }
+
+        public void print()
+        {
+            photo.print();
+            prin = name + " have type " + PhotoType;
+        }
+    }
+}
---
+++ Adapter/Adapter/Form1.cs
@@ -0,0 +1,54 @@
+﻿using gonki;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Adapter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Photo car = new Photo();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Photo PNGphoto = new PNGAdapter(car);
+            PNGphoto.print();
+            richTextBox1.Text += PNGphoto.prin + "\f";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Photo BitmapPhoto = new BitmapAdapter(car);
+            BitmapPhoto.print();
+            richTextBox1.Text += BitmapPhoto.prin + "\f"; ;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Photo JPEGphoto = new JPEGAdapter(car);
+            JPEGphoto.print();
+            richTextBox1.Text += JPEGphoto.prin + "\f";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Photo VectorPhoto = new VectorAdapter(car);
+            VectorPhoto.print();
+            richTextBox1.Text += VectorPhoto.prin + "\f";
+        }
+    }
+}
